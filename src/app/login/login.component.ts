368f49bed641c74c111564c13323f8cf321fd9d0
at-oanhlt <oanhlt@asiantech.vn>
2017-05-26 17:29:00 +0700

    html login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userCode: String;
+  userPassword: String;
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  login() {
+    console.log(this.userCode, this.userPassword);
+
+  }
 }
